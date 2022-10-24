06f2195782ac8c386cf05625c73c0afda6d490aa
Ruairi <G00411334@gmail.ie>
2022-10-24 23:34:36 +0100

    lab05

--- config.py
+++
@@ -1,5 +0,0 @@
-# config file for safe key or password storage.
-# Make a dictionary object:
-config = {
-    "githubkey":"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"
-}
